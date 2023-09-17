bdef3e9625cd03eeb0d0ce4bf4078cf93d8afb10
RF-UoA <a1884959@adelaide.edu.au>
2023-09-17 14:51:34 +0930

    sudo code introduction

--- main.cpp
+++ main.cpp
@@ -14,7 +14,29 @@ using namespace std;
 
 int main(void) {
 
-    cout << "testing!" << endl;
+    // level number = 0
+    // player money = 100
+
+    // generate map
+    // draw map
+    // draw tower
+    // hold game until play button is selected
+
+    // Level loop
+        // level number += 1
+        // add one length to path
+        // spawn enemies based on level number
+
+        // game loop
+            // move all enemies by 1 value
+            // run 'attack' method on all towers (they may only attack if enemies are in range)
+            // if player purchases a new tower, place tower on map
+            // drawWindow function
+                // re-draw map
+                // re-draw towers
+                // draw all enemies
+                // draw all projectiles
+        
 
     return 0;
 }
